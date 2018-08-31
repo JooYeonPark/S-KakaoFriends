b97fdf28072aff4d6df30bb52426c7ee613de864
jooyeon <legm0117@naver.com>
2018-08-31 10:23:24 +0900

    Initialize Project

---
+++ src/main/java/com/kakaoix/mall/SKakaoFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.kakaoix.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SKakaoFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SKakaoFriendsApplication.class, args);
+	}
+
+}

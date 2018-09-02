f0a6dc0d7b453dc720d811f00c80bb9b415f6069
jooyeon <legm0117@naver.com>
2018-09-02 14:29:23 +0900

    # 인터셉터

---
+++ src/main/java/com/kakaoix/mall/interceptor/AuthorityInterceptor.java
@@ -0,0 +1,29 @@
+package com.kakaoix.mall.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component
+public class AuthorityInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		HttpSession session = request.getSession();
+		
+		if(session.getAttribute("memberNo") != null) {
+			return true;
+		}
+		
+		response.sendRedirect("/member/login");
+		
+		return false;
+	}
+	
+
+}
